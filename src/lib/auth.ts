6914c7f9788fd62b37ffe321b7046c476f139df2
sathishkannan162 <sathishkannan162@gmail.com>
2023-07-03 21:23:32 +0530

    style: format auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,17 @@
-import type { Account, NextAuthOptions, Profile, User } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prisma";
-import {signIn} from "next-auth/react";
-import {AdapterUser} from "next-auth/adapters";
 
-if (process.env.GITHUB_CLIENT_ID === undefined || process.env.GITHUB_CLIENT_SECRET===undefined || process.env.GOOGLE_CLIENT_ID===undefined || process.env.GOOGLE_CLIENT_SECRET === undefined ) {
+if (
+  process.env.GITHUB_CLIENT_ID === undefined ||
+  process.env.GITHUB_CLIENT_SECRET === undefined ||
+  process.env.GOOGLE_CLIENT_ID === undefined ||
+  process.env.GOOGLE_CLIENT_SECRET === undefined
+) {
   throw new Error("client id or secret for providers not set");
 }
-
-
 
 export const options: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -19,10 +20,9 @@ export const options: NextAuthOptions = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
 };
-
